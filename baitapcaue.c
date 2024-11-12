fa21b08966c3495d27b49e19cf052c418347cf90
Tuan Khoa Tran Ngo <23130024@student.hcmus.edu.vn>
2024-11-12 22:02:35 +0700

    mới chỉnh lại thành *.c

---
+++ baitapcaue.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+// ham xac dinh vi tri cuoi cung cua so hang
+int chusoovitricuoicung(char *a, int n) {
+    int i = n;
+    while (isdigit(a[i])) { // kiem tra neu la chu so
+        i++;
+    }
+    return i - 1; // tra ve vi tri cuoi cung cua so hang
+}
+
+int main() {
+    char a[] = "(((20-17)*3/3)+9)/3-2)";
+    printf("Bieu thuc : %s\n", a);
+    printf("Vi tri cuoi cung cua cac so hang :\n");
+    for (int i = 0; i < strlen(a); i++) {
+        if (isdigit(a[i])) { // khi gap so hang
+            int ketthuc = chusoovitricuoicung(a, i);
+            printf("so hang bat dau tai %d và ket thuc tai %d\n", i, ketthuc);
+            i = ketthuc; // bo qua cac chu so da duyet 
+        }
+    }
+
+    return 0;
+}
